6568f27a0e82214f92bf0a527fc3a4f1e5e33106
g233liu <g233liu@uwaterloo.ca>
2022-03-21 17:30:44 +0800

    upload

--- submit_ppo_highD_q_vector.sh
+++ submit_ppo_highD_q_vector.sh
@@ -14,4 +14,4 @@ conda activate galen-cr37
 cd ./interface/
 #python train_commonroad_ppo.py ../config/train_ppo_highD_no_collision.yaml -p 1 -s 123 -l "$log_dir"
 #python train_commonroad_ppo.py ../config/train_ppo_highD_no_collision.yaml -n 5 -s 321 -l "$log_dir"
-python train_commonroad_ppo.py ../config/train_ppo_highD_velocity_penalty.yaml -n 5 -s 123 -l "$log_dir"
+python train_commonroad_ppo.py ../config/train_ppo_highD_no_velocity_penalty.yaml -n 5 -s 123 -l "$log_dir"
