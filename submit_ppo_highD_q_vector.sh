f73040ee596f07916df208d4517d2fe402baa79f
g233liu <g233liu@uwaterloo.ca>
2022-04-04 13:46:39 +0800

    upload

--- submit_ppo_highD_q_vector.sh
+++ submit_ppo_highD_q_vector.sh
@@ -13,5 +13,5 @@ source /h/galen/miniconda3/bin/activate
 conda activate galen-cr37
 cd ./interface/
 #python train_ppo.py ../config/train_ppo_highD_no_velocity_penalty_bs--1_fs-5k_nee-10.yaml -n 5 -s 123 -l "$log_dir"
-python train_ppo.py ../config/train_ppo_highD_velocity_penalty_bs--1_fs-5k_nee-10_lr-5e-4_vm-45.yaml -n 5 -s 123 -l "$log_dir"
-#python train_ppo.py ../config/train_ppo_highD_velocity_penalty_bs--1_fs-5k_nee-10_lr-5e-4_vm-50.yaml -n 5 -s 123 -l "$log_dir"
+#python train_ppo.py ../config/train_ppo_highD_velocity_penalty_bs--1_fs-5k_nee-10_lr-5e-4_vm-45.yaml -n 5 -s 123 -l "$log_dir"
+python train_ppo.py ../config/train_ppo_highD_velocity_penalty_bs--1_fs-5k_nee-10_lr-5e-4_vm-50.yaml -n 5 -s 123 -l "$log_dir"
