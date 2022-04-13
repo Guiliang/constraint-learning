6c3cef0aef7f737a410bf68e9b96404e69971080
g233liu <g233liu@uwaterloo.ca>
2022-04-14 00:41:37 +0800

    upload

--- submit_icrl_highD_q_vector.sh
+++ submit_icrl_highD_q_vector.sh
@@ -12,5 +12,5 @@ log_dir="log-${task_name}-${launch_time}.out"
 source /h/galen/miniconda3/bin/activate
 conda activate galen-cr37
 cd ./interface/
-python train_icrl.py ../config/highD_velocity_constraint/train_ICRL_highD_velocity_constraint_no_is_bs--1-5e2_fs-5k_nee-10_lr-5e-4_no-buffer_vm-40.yaml -s 123 -n 5 -l "$log_dir"
-#python train_icrl.py ../config/highD_velocity_constraint/train_ICRL_highD_velocity_constraint_no_is_bs--1-1e3_fs-5k_nee-10_lr-5e-4_no-buffer_vm-40.yaml -s 123 -n 5 -l "$log_dir"
+#python train_icrl.py ../config/highD_velocity_constraint/train_ICRL_highD_velocity_constraint_no_is_bs--1-5e2_fs-5k_nee-10_lr-5e-4_no-buffer_vm-40.yaml -s 123 -n 5 -l "$log_dir"
+python train_icrl.py ../config/highD_velocity_constraint/train_ICRL_highD_velocity_constraint_no_is_bs--1-1e3_fs-5k_nee-10_lr-5e-4_no-buffer_vm-40.yaml -s 123 -n 5 -l "$log_dir"
