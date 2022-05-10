4ed6a4beefdd0cf5dd677a26eab2a53a90f3f86a
g233liu <g233liu@uwaterloo.ca>
2022-05-11 00:53:03 +0800

    2022-05-11

--- submit_gail_mujuco_v_vector.sh
+++ submit_gail_mujuco_v_vector.sh
@@ -19,7 +19,7 @@ source /pkgs/anaconda3/bin/activate
 conda activate cn-py37
 pip install -e ./mujuco_environment
 cd ./interface
-python train_gail.py ../config/mujoco_InvertedPendulumWall-v0/train_GAIL_InvertedPendulumWall-v0.yaml -n 5 -s 123 -l "$log_dir"
-process_id=$!
-wait $process_id
+python train_gail.py ../config/mujoco_InvertedPendulumWall-v0/train_GAIL_disclr-1e-5_InvertedPendulumWall-v0.yaml -n 5 -s 123 -l "$log_dir"
+#process_id=$!
+#wait $process_id
 echo shell finish running
