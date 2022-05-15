7857835e0b084c4438b8152e66a3037073a94339
g233liu <g233liu@uwaterloo.ca>
2022-05-15 17:26:39 +0800

    update

--- submit_vicrl_mujuco_v_vector_2.sh
+++ submit_vicrl_mujuco_v_vector_2.sh
@@ -19,5 +19,5 @@ source /pkgs/anaconda3/bin/activate
 conda activate cn-py37
 pip install -e ./mujuco_environment
 cd ./interface
-python train_icrl.py ../config/mujoco_InvertedPendulumWall-v0/train_VICRL_InvertedPendulumWall-v0_prl-1e-2_lr-3e-5_p-1e-2-9e-2.yaml -n 5 -s 123 -l "$log_dir"
+python train_icrl.py ../config/mujoco_InvertedPendulumWall-v0/train_VICRL_InvertedPendulumWall-v0_prl-1e-2_lr-3e-5_p-9e-2-1e-2.yaml -n 5 -s 123 -l "$log_dir"
 echo shell finish running
