33b045025280131165ec988c8a8fe48e529d4565
Guiliang <g233liu@uwaterloo.ca>
2022-07-21 23:49:38 +0800

    half-cheetah

--- submit_mixture_ppo_lag_mujuco_v_vector.sh
+++ submit_mixture_ppo_lag_mujuco_v_vector.sh
@@ -18,5 +18,5 @@ conda activate cn-py37
 pip install -e ./mujuco_environment
 cd ./interface/
 python train_ppo.py ../config/mujuco_mixture_HCWithPos-v0/train_me_c-0_ppo_lag_HCWithPos-v0.yaml -n 5 -s 123 -l "$log_dir"
-#python train_ppo.py ../config/mujuco_mixture_HCWithPos-v0/train_me_c-1_ppo_lag_HCWithPos-v0.yaml -n 5 -s 123 -l "$log_dir"
+python train_ppo.py ../config/mujuco_mixture_HCWithPos-v0/train_me_c-1_ppo_lag_HCWithPos-v0.yaml -n 5 -s 123 -l "$log_dir"
 echo shell finish running
