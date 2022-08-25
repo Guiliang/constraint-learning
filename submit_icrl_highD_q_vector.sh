3e92c47ef42d59017dfe020ee97676d621233350
g233liu <g233liu@uwaterloo.ca>
2022-08-25 20:10:37 +0800

    2022-8-25

--- submit_icrl_highD_q_vector.sh
+++ submit_icrl_highD_q_vector.sh
@@ -12,4 +12,4 @@ log_dir="log-${task_name}-${launch_time}.out"
 source /h/galen/miniconda3/bin/activate
 conda activate galen-cr37
 cd ./interface/
-python train_icrl.py ../config/highD_velocity_distance_constraint/train_ICRL_highD_velocity_distance_constraint_p-9e-1-1e-1_no_is_bs--1-5e2_fs-5k_nee-10_lr-5e-4_no-buffer_vm-40_dm-20.yaml -s 123 -n 5 -l "$log_dir"
+python train_icrl.py ../config/highD_velocity_distance_constraint/train_ICRL_highD_velocity_distance_constraint_bs--1-5e2_fs-5k_nee-10_lr-5e-4_no-buffer_vm-40_dm-20.yaml -s 123 -n 5 -l "$log_dir"
