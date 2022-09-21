d6fb71e35def4830a3e72e3ac5381cbae53de921
g233liu <g233liu@uwaterloo.ca>
2022-09-22 01:00:14 +0800

    2022-09-21

--- submit_collect_data_highD_q_vector.sh
+++ submit_collect_data_highD_q_vector.sh
@@ -12,6 +12,6 @@ log_dir="log-${task_name}-${launch_time}.out"
 source /h/galen/miniconda3/bin/activate
 conda activate galen-cr37
 cd ./interface/
-python generate_data_for_constraint_inference.py -n 5 -rn 0 -mn train_ppo_lag_highD_velocity_distance_penalty_bs--1_fs-5k_nee-10_lr-5e-4_vm-40_dm-20-multi_env-Aug-15-2022-07:55-seed_123 -tn PPO-Lag-highD-velocity-distance -ct no-too_closed-over_speed-off_road
+python generate_data_for_constraint_inference.py -n 5 -rn 0 -mn train_ppo_lag_highD_velocity_penalty_bs--1_fs-5k_nee-10_lr-5e-4_vm-40-multi_env-Sep-19-2022-05:46-seed_123 -tn PPO-Lag-highD-velocity -ct no-over_speed-off_road
 #python generate_data_for_constraint_inference.py -n 5 -rn 0 -mn train_ppo_lag_highD_no_slo_distance_penalty_bs--1_fs-5k_nee-10_lr-5e-4_dm-10-multi_env-May-24-2022-00:53-seed_123 -tn PPO-Lag-highD-distance -ct no-too_closed
 #python generate_data_for_constraint_inference.py -n 5 -rn 0 -mn train_ppo_lag_highD_no_slo_distance_penalty_bs--1_fs-5k_nee-10_lr-5e-4_dm-20-multi_env-May-24-2022-00:53-seed_123 -tn PPO-Lag-highD-distance -ct no-too_closed
