ee8d7b8f81fffb0b577908211eeb15291bd398f7
g233liu <g233liu@uwaterloo.ca>
2022-11-10 16:37:14 +0800

    2022-11-10

--- server_run_vcirl_3.sh
+++ server_run_vcirl_3.sh
@@ -1,4 +1,4 @@
-task_name="train-vicrl_2"
+task_name="train-vicrl_3"
 launch_time=$(date +"%m-%d-%y-%H:%M:%S")
 log_dir="log-server-${task_name}-${launch_time}.out"
 source /data/Galen/miniconda3-4.12.0/bin/activate
@@ -9,8 +9,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/Galen/project-constraint-learning-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 pip install -e ./mujuco_environment/
 cd ./interface/
-export CUDA_VISIBLE_DEVICES=2
-python train_icrl.py ../config/mujuco_HCWithPos-v0/train_VICRL_HCWithPos-v0_with_action_with_buffer_p-9e-1-1e-1_clr-5e-3_no_is_noise-5e-4.yaml -n 5 -s 321 -l "$log_dir"
-python train_icrl.py ../config/mujuco_HCWithPos-v0/train_VICRL_HCWithPos-v0_with_action_with_buffer_p-9e-1-1e-1_clr-5e-3_no_is_noise-5e-4.yaml -n 5 -s 666 -l "$log_dir"
-
+export CUDA_VISIBLE_DEVICES=3
+python train_icrl.py ../config/mujuco_HCWithPos-v0/train_VICRL_HCWithPos-v0_with_action_with_buffer_p-9e-1-1e-1_clr-5e-3_no_is_piv-1e1_noise-5e-3.yaml -n 5 -s 321 -l "$log_dir"
+python train_icrl.py ../config/mujuco_HCWithPos-v0/train_VICRL_HCWithPos-v0_with_action_with_buffer_p-9e-1-1e-1_clr-5e-3_no_is_piv-1e1_noise-5e-3.yaml -n 5 -s 666 -l "$log_dir"
 cd ../
--- server_run_vcirl_4.sh
+++ server_run_vcirl_4.sh
@@ -1,4 +1,4 @@
-task_name="train-vicrl_1"
+task_name="train-vicrl_4"
 launch_time=$(date +"%m-%d-%y-%H:%M:%S")
 log_dir="log-server-${task_name}-${launch_time}.out"
 source /data/Galen/miniconda3-4.12.0/bin/activate
@@ -9,8 +9,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/Galen/project-constraint-learning-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 pip install -e ./mujuco_environment/
 cd ./interface/
-export CUDA_VISIBLE_DEVICES=1
-python train_icrl.py ../config/mujoco_Circle-v0/train_VICRL_Circle_dim2.yaml -n 5 -s 123 -l "$log_dir"
-python train_icrl.py ../config/mujoco_Circle-v0/train_VICRL_Circle_dim2.yaml -n 5 -s 321 -l "$log_dir"
-python train_icrl.py ../config/mujoco_Circle-v0/train_VICRL_Circle_dim2.yaml -n 5 -s 666 -l "$log_dir"
+export CUDA_VISIBLE_DEVICES=0
+python train_icrl.py ../config/mujuco_HCWithPos-v0/train_VICRL_HCWithPos-v0_with_action_with_buffer_p-9e-1-1e-1_clr-5e-3_no_is_noise-1e-3.yaml -n 5 -s 321 -l "$log_dir"
+python train_icrl.py ../config/mujuco_HCWithPos-v0/train_VICRL_HCWithPos-v0_with_action_with_buffer_p-9e-1-1e-1_clr-5e-3_no_is_noise-1e-3.yaml -n 5 -s 666 -l "$log_dir"
 cd ../
