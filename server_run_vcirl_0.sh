10c93f3006ad40af91ce061ea50707e105211068
Guiliang <22108101+Guiliang@users.noreply.github.com>
2022-11-19 20:34:24 +0800

    2022 11.19

--- server_run_vcirl_0.sh
+++ server_run_vcirl_0.sh
@@ -1,11 +1,11 @@
-task_name="train-vicrl_0"
+task_name="train-meicrl_0"
 launch_time=$(date +"%m-%d-%y-%H:%M:%S")
 log_dir="log-server-${task_name}-${launch_time}.out"
 source /data/Galen/miniconda3-4.12.0/bin/activate
 source activate cn-py37
-export MUJOCO_PY_MUJOCO_PATH=/data/Galen/project-constraint-learning-benchmark/constraint-learning/.mujoco/mujoco210
+export MUJOCO_PY_MUJOCO_PATH=/data/Galen/project-constraint-learning-mixture-experts/constraint-learning/.mujoco/mujoco210
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/Galen/project-constraint-learning-benchmark/constraint-learning/.mujoco/mujoco210/bin:/usr/lib/nvidia
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/Galen/project-constraint-learning-mixture-experts/constraint-learning/.mujoco/mujoco210/bin:/usr/lib/nvidia
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 pip install -e ./mujuco_environment/
 cd ./interface/
