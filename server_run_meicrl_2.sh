271b46987cb598dc418d9c4e3dc7ae2025d668aa
Guiliang <22108101+Guiliang@users.noreply.github.com>
2022-11-19 21:02:24 +0800

    2022 11.19

---
+++ server_run_meicrl_2.sh
@@ -0,0 +1,14 @@
+task_name="train-meicrl_0"
+launch_time=$(date +"%m-%d-%y-%H:%M:%S")
+log_dir="log-server-${task_name}-${launch_time}.out"
+source /data/Galen/miniconda3-4.12.0/bin/activate
+source activate cn-py37
+export MUJOCO_PY_MUJOCO_PATH=/data/Galen/project-constraint-learning-mixture-experts/constraint-learning/.mujoco/mujoco210
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/Galen/project-constraint-learning-mixture-experts/constraint-learning/.mujoco/mujoco210/bin:/usr/lib/nvidia
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+pip install -e ./mujuco_environment/
+cd ./interface/
+export CUDA_VISIBLE_DEVICES=0
+python train_meicrl.py ../config/mujuco_mixture_HCWithPos-v0/train_MEICRL_HCWithPos-v0_cbs-64_lr-5e-5_ft-2e5_exp-neg-coef-5e-1_piv-1e1_plr-1e-2_weight-3e-1_robust-2e-1_advloss.yaml -n 5 -s 123 -l "$log_dir"
+cd ../
