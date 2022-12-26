bcec1719b6f09c363a74ef88c5acf7225c760cac
Guiliang <liuguiliang@cuhk.edu.cn>
2022-12-26 19:14:20 +0800

    2022-12-26

--- local_run_ppo-lag.sh
+++ local_run_ppo-lag.sh
@@ -7,7 +7,6 @@ export MUJOCO_PY_MUJOCO_PATH=/scratch1/PycharmProjects/constraint-learning-bench
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/scratch1/PycharmProjects/constraint-learning-benchmark/.mujoco/mujoco210/bin:/usr/lib/nvidia
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 cd ./interface/
-nohup python train_ppo.py ../config/mujoco_Circle-v0/train_ppo_lag_Circle_id-0_dim2.yaml -n 5 -l "$log_dir" > nohup.out 2>&1 &
-#nohup python train_policy.py ../config/mujoco_Circle-v0/train_ppo_lag_Circle_id-1_dim2.yaml -n 5 -l "$log_dir" > nohup.out 2>&1 &
-#nohup python train_policy.py ../config/mujoco_Circle-v0/train_ppo_lag_Circle_id-2_dim2.yaml -n 5 -l "$log_dir" > nohup.out 2>&1 &
+
+nohup python train_ppo.py ../config/mujoco_WGW-v0/train_ppo_lag_WGW-v0_max-nu-1-setting4.yaml -n 1 -l "$log_dir" > nohup.out 2>&1 &
 cd ../
